ad6552ade15b593cd57247bf663aadf97184c96d
maxamx <maxamx@gmail.com>
2021-09-24 09:36:01 -0300

    config customer

---
+++ src/main/java/maxamx/model/Customer.java
@@ -0,0 +1,31 @@
+package maxamx.model;
+
+import java.math.BigInteger;
+
+public class Customer {
+
+    public Long id;
+    public String name;
+    public String lastName;
+    public String document;
+
+    public Customer(){}
+
+    public Customer(Long id, String name, String lastName, String document) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.document = document;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", document='" + document + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/maxamx/producer/CustomerResource.java
@@ -0,0 +1,38 @@
+package maxamx.producer;
+
+import io.smallrye.mutiny.Multi;
+import maxamx.model.Customer;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.UUID;
+
+@Path("/customers")
+public class CustomerResource {
+    @Channel("2mgt1okd-default")
+    Emitter<String> customerRequestEmiter;
+
+    @POST
+    @Path("/request")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String createRequest(){
+        UUID uuid = UUID.randomUUID();
+        customerRequestEmiter.send(uuid.toString());
+        return uuid.toString();
+    }
+
+    @Channel("2mgt1okd")
+    Multi<Customer> customers;
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public Multi<Customer> stream(){
+        return customers;
+    }
+
+}
